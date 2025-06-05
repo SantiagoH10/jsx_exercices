c408e9aa11f802e7410bf583785272c919320d27
SantiagoH10 <santiago.hernandez-bernal@outlook.com>
2025-06-05 13:29:26 +0000

    first jsx exercices

--- jsx-exercices/src/App.jsx
+++ jsx-exercices/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
+
+function WelcomeMessage() {
+  return (
+    <div>
+      <h3>Welcome!</h3>
+      <p>Hello, Santiago!</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+function UserCard({name, age, email}) {
+  return (
+    <div>
+      <p>Your name is: {name}</p>
+      <p>Your age is: {age}</p>
+      <p>Your email is: {email}</p>
+    </div>
+  )
+}
+
+//First possibility - Verbose and messy
+/*
+function LoginStatus () {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  if (isLoggedIn === true) {
+    return (
+      <div>
+        <h2>Welcome back!</h2>
+        <button onClick={()=>setIsLoggedIn(false)}>Log out</button>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>Please log in</h2>
+        <button onClick={()=>setIsLoggedIn(true)}>Log In</button>
+      </div>
+    )
+  }
+}
+*/
+
+function LoginStatus() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return(
+    <div>
+      <p>{isLoggedIn ? "Welcome back!" : "Please log in"}</p>
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        {isLoggedIn ? "Logout" : "Login"}
+      </button>
+    </div>
+  )
+}
+
+function FruitList() {
+  const fruits = ['apple', 'banana', 'orange', 'grape'];
+  return(
+    <ul>
+      {fruits.map(f => <li>{f}</li> )}
+    </ul>
+  )
+}
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <WelcomeMessage />
+      <UserCard name="Joe" age="25" email="foo@gmail.com" />
+      <LoginStatus />
+      <FruitList />
+    </div>
+  );
 }
 
 export default App
