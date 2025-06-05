ae7e1d22c11f2309b950db9d1b0e73b5f7f8609c
SantiagoH10 <santiago.hernandez-bernal@outlook.com>
2025-06-05 21:20:05 +0200

    set up for number-guessing-game

---
+++ number-guessing-game/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="bg-red-500 p-4">
+      <p className='text-green'>Hello World</p>
+    </div>
+  )
+}
+
+export default App
