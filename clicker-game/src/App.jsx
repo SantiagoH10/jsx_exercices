b5bae31d7d9d8f3af678762ea7639192ca2d1246
SantiagoH10 <santiago.hernandez-bernal@outlook.com>
2025-06-10 15:03:33 +0000

    set up clicker game

---
+++ clicker-game/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import cma from './assets/cma.png'
+import { useState, useRef, useEffect } from 'react'
+
+function MySociabble() {
+  return(
+    <div className="bg-white text-ccblue p-6">
+      <h1 className="text-3xl font-semibold mb-4">My Sociabble</h1>
+      <img 
+        src={cma} 
+        alt="CMA CGM Logo" 
+        className="w-32 h-auto"
+      />
+    </div>
+  )
+}
+
+function ClickerGame() {
+  
+}
+
+
+function App() {
+  return (
+    <MySociabble />
+  )
+}
+
+export default App
