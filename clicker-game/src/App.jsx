bce9a36c26650c3d6f0a6e80f2fe7c5e5cd603e2
SantiagoH10 <santiago.hernandez-bernal@outlook.com>
2025-06-10 16:03:42 +0000

    click button working

--- clicker-game/src/App.jsx
+++ clicker-game/src/App.jsx
@@ -16,13 +16,42 @@ function MySociabble() {
 }
 
 function ClickerGame() {
+  const [points, setPoints] = useState(0);
+  const [pointPerClick, setPointsPerClick] = useState(1);
+  const [pointsPerSecond, setPointsPerSecond] = useState(0);
   
+  return(
+    <div className="flex">
+      <div className="m-4 bg-white p-12 rounded-lg shadow-lg border border-gray-200 w-96 h-96 flex flex-col items-center justify-center">
+        <p className="text-1xl font-bold text-gray-800 mb-8 text-center">
+          Welcome
+        </p>
+        <p className="text-1xl font-bold text-gray-800 mb-8 text-center">
+          Total points : {points}
+        </p>
+        <button 
+          className="w-24 h-24 bg-blue-500 hover:bg-blue-600 active:scale-90 text-white font-semibold rounded-full transition-transform duration-25 ease-out shadow-md hover:shadow-lg flex items-center justify-center transform hover:scale-105 select-none"
+          onClick={()=>setPoints(prevPoints => prevPoints + pointPerClick)}
+        >
+          Click Me!
+        </button>
+      </div>
+      <div className="m-4 bg-white p-12 rounded-lg shadow-lg border border-gray-200 w-32 h-96 flex items-center justify-center">
+        <p className="text-1xl font-bold text-gray-800 mb-8 text-center">
+          Shop
+        </p>
+      </div>
+    </div>
+  )
 }
 
 
 function App() {
   return (
-    <MySociabble />
+    <div>
+      <MySociabble/>
+      <ClickerGame/>
+    </div>
   )
 }
 
