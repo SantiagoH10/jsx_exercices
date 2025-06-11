677698df8f02007b7715c38f0ed07c78000b87ae
SantiagoH10 <santiago.hernandez-bernal@outlook.com>
2025-06-11 16:30:02 +0000

    clicker game

--- clicker-game/src/App.jsx
+++ clicker-game/src/App.jsx
@@ -15,11 +15,71 @@ function MySociabble() {
   )
 }
 
+class ClickUpgrade {
+  constructor(level, name, cost, improvement) {
+    this.level = level,
+    this.name = name,
+    this.cost = cost,
+    this.improvement = improvement,
+    this.improvShop = `+${improvement} clicks`
+  }
+}
+
+class AutoUpgrade {
+  constructor(level, name, cost, improvement) {
+    this.level = level;
+    this.name = name;
+    this.cost = cost;
+    this.improvement = improvement;
+    this.improvShop = `+${improvement} per second`;
+  }
+}
+
+const clickUpgrades = [
+  new ClickUpgrade(1, "Power click", 10, 1), 
+  new ClickUpgrade(2, "Click master", 20, 3)
+];
+
+const autoUpgrades = [
+  new AutoUpgrade(1, "Basic Auto-Clicker", 50, 1),
+  new AutoUpgrade(2, "Advanced Auto-Clicker", 200, 5)
+];
+
 function ClickerGame() {
   const [points, setPoints] = useState(0);
   const [pointPerClick, setPointsPerClick] = useState(1);
   const [pointsPerSecond, setPointsPerSecond] = useState(0);
+  const [upgrades, setUpgrades] = useState({click: 0, auto: 0})
   
+  function purchaseUpgrade(type, upgradeIndex) {
+    if (type === "click") {
+      console.log(upgradeIndex);
+      if (upgradeIndex >= clickUpgrades.length) return;
+      
+      setPoints(prevPoints => prevPoints - clickUpgrades[upgradeIndex].cost);
+      setPointsPerClick(prevPointPerClick =>
+        prevPointPerClick + clickUpgrades[upgradeIndex].improvement
+      );
+      console.log(upgrades.click);
+      setUpgrades(prevUpgrades => ({
+        ...prevUpgrades,
+        click: prevUpgrades.click + 1
+      }));
+    } else if (type === "auto") {
+      if (upgradeIndex >= autoUpgrades.length) return;
+      
+      setPoints(prevPoints => prevPoints - autoUpgrades[upgradeIndex].cost);
+      setUpgrades(prevUpgrades => ({
+        ...prevUpgrades,
+        auto: prevUpgrades.auto + 1
+      }));
+      setPointsPerSecond(prevPointPerSec =>
+        prevPointPerSec + autoUpgrades[upgradeIndex].improvement
+      );
+    }
+    console.log(upgrades.click);
+  }
+
   return(
     <div className="flex">
       <div className="m-4 bg-white p-12 rounded-lg shadow-lg border border-gray-200 w-96 h-96 flex flex-col items-center justify-center">
@@ -36,10 +96,35 @@ function ClickerGame() {
           Click Me!
         </button>
       </div>
-      <div className="m-4 bg-white p-12 rounded-lg shadow-lg border border-gray-200 w-32 h-96 flex items-center justify-center">
+      <div id="shop container" className="m-4 bg-white p-12 rounded-lg shadow-lg border border-gray-200 w-64 h-96 flex-column justify-center">
         <p className="text-1xl font-bold text-gray-800 mb-8 text-center">
           Shop
         </p>
+        <div className="bg-gray-50 p-4 mb-1 rounded-md border border-gray-100 space-y-4">
+          <p className="text-xl font-bold text-gray-800 mb-4">
+            Improve click
+          </p>
+          <button 
+            disabled={upgrades.click >= clickUpgrades.length || points < clickUpgrades[upgrades.click].cost}
+            className={upgrades.click < clickUpgrades.length && points >= clickUpgrades[upgrades.click].cost
+              ? "bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200 w-full"
+              : "bg-gray-400 text-gray-600 cursor-not-allowed font-semibold py-2 px-4 rounded-lg transition-colors duration-200 w-full"
+            }
+            onClick={() => purchaseUpgrade("click", upgrades.click)}
+          >
+            {upgrades.click < clickUpgrades.length ? "Buy" : "Max level"}
+          </button>
+        </div>
+        <div className="bg-gray-50 p-4 rounded-md border border-gray-100 space-y-4">
+          <p className="text-xl font-bold text-gray-800 mb-4">
+            Auto click
+          </p>
+          <button 
+            className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200 w-full"
+          >
+            Buy
+          </button>
+        </div>
       </div>
     </div>
   )
